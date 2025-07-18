159c15cef827f04ffa0645259bf882de6c99d7f7
Ludwin FM <ludwinss@gmail.com>
2025-07-18 01:02:57 -0400

    feat(windsurf): add config windsfurd

---
+++ lua/plugins/languages/windsfurd-nvim.lua
@@ -0,0 +1,16 @@
+require("codeium").setup({
+	virtual_text = {
+		enabled = true,
+		idle_delay = 75,
+		key_bindings = {
+			accept = "<Tab>",
+			next = "<M-]>",
+			prev = "<M-[>",
+			clear = "<C-]>",
+			accept_word = "<C-j>",
+			accept_line = "<C-k>",
+		},
+	},
+
+	enable_cmp_source = true,
+})
