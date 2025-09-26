2b23359b38c0f5b1570e7652902387d3e7546c51
Ludwin FM <ludwinss@gmail.com>
2025-09-26 15:47:57 -0400

    build(nvim): :package: added sql-formatter to mason tool installer

--- lua/plugins/languages/mason-tools.lua
+++ lua/plugins/languages/mason-tools.lua
@@ -3,6 +3,7 @@ require("mason-tool-installer").setup({
 		-- "csharpier", Ahora usamos el dotnet tools install csharpier
 		"stylua",
 		"prettierd",
+		"sql-formatter",
 	},
 	auto_update = true,
 	run_on_start = true,
